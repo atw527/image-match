99ba686c6446e42c86349b50b1f9825221db507f
Andrew Wells <andrew@wellsie.net>
2017-10-31 08:40:02 -0700

    ffmpeg command; 1080p mp4 quality update
    1080p is fine for image template matching...don't want 4k

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -9,5 +9,9 @@ fi
 chmod a+rx /usr/local/bin/youtube-dl
 
 youtube-dl --write-thumbnail --write-description --write-info-json --restrict-filenames \
-    -o '/usr/test/%(upload_date)s_%(id)s.%(ext)s' -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' \
+    -o '/usr/test/%(upload_date)s_%(id)s.%(ext)s' -f 137 \
     -a /usr/local/bin/queue.txt
+
+mkdir /usr/test/l-_NYHkKdwQ
+
+#ffmpeg -i /usr/test/20170817_l-_NYHkKdwQ.mp4 -r 15/1 -f image2 /usr/test/l-_NYHkKdwQ/l-_NYHkKdwQ.%6d.jpg
