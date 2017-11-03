6fcee36d8255bb4a4749d0c83cd7751c15a338a2
Andrew Wells <andrew@wellsie.net>
2017-11-02 19:54:21 -0700

    Download script

--- data/bin/video-download.sh
+++ data/bin/video-download.sh
@@ -12,11 +12,11 @@ youtube-dl --write-thumbnail --write-description --write-info-json --restrict-fi
     -o '/usr/local/video/%(id)s.%(ext)s' -f 137 \
     -a /usr/local/bin/queue.txt
 
-cd /usr/local/video/
+cd /usr/local/frames/
 for f in *.mp4
 do
     dir="${f%.*}"
-	if [ ! -d $dir ]; then
+	if [ ! -d "$dir" ]; then
         echo $dir not found
         mkdir /usr/local/frames/$dir
         ffmpeg -i /usr/local/video/$f -r 10/1 -f image2 /usr/local/frames/$dir/%6d.jpg
