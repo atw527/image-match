e2e18bd8ab1ff792f02a1512769c4a362be379d8
Andrew Wells <andrew@wellsie.net>
2017-11-05 07:48:37 -0800

    Changing front server

--- match.py
+++ match.py
@@ -52,7 +52,7 @@ query = "UPDATE tasks SET worker_host = %s, started = %s WHERE task_id = %s LIMI
 args = (hostname, time.strftime('%Y-%m-%d %H:%M:%S'), task_id)
 x.execute(query, args)
 
-os.system("wget -O /tmp/" + row[3] + " http://server-10:8088/templates/" + row[3])
+os.system("wget -O /tmp/" + row[3] + " http://a01-docker-02:8088/templates/" + row[3])
 
 img1 = cv2.imread(source_image, 0)          # queryImage
 
