c52964bce651906e2b24a6be2b3b232e1f4d7c81
Andrew Wells <andrew@wellsie.net>
2017-11-04 17:11:46 -0700

    directory maze

--- data/bin/frame-dedup.sh
+++ data/bin/frame-dedup.sh
@@ -2,14 +2,15 @@
 
 cd "${0%/*}"
 cd ..
+cd video
 
-for f in ./video/*.mp4
+for f in *.mp4
 do
     dir="${f%.*}"
-	if [ -d "./frames/$dir" ]; then
+	if [ -d "../frames/$dir" ]; then
         uptime
         echo $dir filter out duplicate frames...
-        ./bin/duplicates.php "./frames/$dir"
+        ../bin/duplicates.php "../frames/$dir"
         echo $dir filter complete!
     else
         echo "$dir does not exist for some reason"
