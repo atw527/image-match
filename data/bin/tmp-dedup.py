65f0e9f6666f05bc43c2e624f6d4641ed569e0be
Andrew Wells <andrew@wellsie.net>
2017-11-07 13:24:06 -0800

    Test script

--- data/bin/tmp-dedup.py
+++ data/bin/tmp-dedup.py
@@ -1,13 +1,22 @@
 
-video_id = ruZ2v27KHCE
+import os
+import re
+import sys
+import os.path
+import subprocess
+
+video_id = "ruZ2v27KHCE"
 
 # dedup
 print "[{0}] Running dedup...".format(video_id)
-frames = os.listdir("frames/{0}".format(video_id))
+
+os.chdir("frames/{0}".format(video_id))
+frames = os.listdir(".")
 
 for x in range(0, len(frames) - 1):
     for y in range(x + 1, len(frames)):
         try:
+            # subprocess.run?
             output = subprocess.check_output("compare -metric RMSE {0} {1} NULL: 2>&1".format(frames[x], frames[y]), shell=True)
             diff = int(re.search('[0-9]+', output).group())
 
@@ -15,12 +24,13 @@ for x in range(0, len(frames) - 1):
 
             if diff < 5000 and diff != 0:
                 # frame is similar enough to remove
-                print "del frames/{0}.jpg".format(frames[y]))
+                print "del frames/{0}.jpg".format(frames[y])
             else:
                 # frame has changed, set this as the new starting point
                 x = y
                 print "frames different"
                 break
         except Exception, e:
+            print str(e)
             print "exception"
             continue
