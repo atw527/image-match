97cd9f019158f05a606f6f2492ead66edff149ee
Andrew Wells <awells@kochava.com>
2017-11-09 11:24:02 -0800

    Allow dynamic DLing off frames

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ This image will download the tools to process the video downloads and frame extr
 Since the only output is in stdout of docker-compose, ffmpeg and youtube-dl don't report progress very well.  Run `du -sh` on the data folders to see their current size and get an idea of progress.  The `video` folder will grow first, followed by the `frames` folder.
 
 Once complete, there will be a rather large `./data/video` and `./data/frames` folders.
+
+# Server Layout
+
+## Master
+
+Master contains a copy of all data - video MP4, meta, and frames.  The current data size is 190G total.
+
+The `front` instance powers the web interface and should be ran on the server box.
+
+## Slaves
+
+Slaves don't need to contain anything in `data/video`.  Tasks will run faster if they are able to hold copies of `data/frames`, requiring > 50G available storage.
--- match.py
+++ match.py
@@ -11,13 +11,17 @@ import numpy as np
 from functools import partial
 from matplotlib import pyplot as plt
 
-def signal_handler(task_id, x, conn, signal, frame):
+def signal_handler(task_id, x, conn, signal, frame, dl_required):
         print('Shuting down...')
 
         query = "DELETE FROM image_matches_bf WHERE task_id = " + str(task_id)
         x.execute(query)
         query = "UPDATE tasks SET worker_host = null, started = null, completed = null WHERE task_id = " + str(task_id) + " LIMIT 1"
         x.execute(query)
+
+        if video_id != "" and dl_required:
+            os.system("rm -f data/frames/" + video_id + "/*.jpg")
+            os.system("rmdir data/frames/" + video_id)
 
         sys.exit(0)
 
@@ -29,49 +33,66 @@ dirs = next(os.walk('data/frames'))[1]
 dir_list = "'" + "', '".join(dirs) + "'"
 
 while True:
+    # first try to pickup a task where we hold the frames
     sql = "SELECT task_id, guid, video_id, template FROM tasks WHERE started IS NULL && video_id IN (" + dir_list + ") LIMIT 1"
     x.execute(sql)
     if x.rowcount == 1:
+        dl_required = False
         break
+
+    # if there are no tasks available, offer to pickup one that requires a rsync
+    sql = "SELECT task_id, guid, video_id, template FROM tasks WHERE started IS NULL LIMIT 1"
+    x.execute(sql)
+    if x.rowcount == 1:
+        dl_required = True
+        break
+
     time.sleep(5)
 
 row = x.fetchone()
 
 task_id = row[0]
 task_guid = row[1]
-youtube_id = row[2]
-youtube_path = "data/frames/" + youtube_id + "/"
+video_id = row[2]
+video_path = "data/frames/" + video_id + "/"
 source_image = "/tmp/" + row[3]
 exceptions = 0
 
 if os.path.isfile("/etc/docker_hostname"):
     hostname = open("/etc/docker_hostname").read()
+    hostname = hostname.strip()
     container = socket.gethostname()
 else:
     hostname = socket.gethostname()
-    container = ""
+    container = None
 
 query = "UPDATE tasks SET worker_host = %s, container = %s, started = %s WHERE task_id = %s LIMIT 1"
 args = (hostname, container, time.strftime('%Y-%m-%d %H:%M:%S'), task_id)
 x.execute(query, args)
 
-os.system("wget -O /tmp/" + row[3] + " http://server-13:8088/templates/" + row[3])
+start_time = time.time()
+print "Picking up task: ", task_id, task_guid, video_id
 
+signal.signal(signal.SIGINT, partial(signal_handler, task_id, x, conn, dl_required))
+signal.signal(signal.SIGTERM, partial(signal_handler, task_id, x, conn, dl_required))
+
+if dl_required:
+    # fetch copy of frames
+    print "[{0}] Copying frames from master...".format(video_id)
+    os.system("rsync -a andrew@server-13:/home/andrew/go/src/github.com/atw527/image-match/data/frames/ data/frames/{0}".format(video_id))
+
+# fetch template image
+print "[{0}] Copying image template from master...".format(video_id)
+os.system("wget -O /tmp/" + row[3] + " http://server-13:8088/templates/" + row[3])
 img1 = cv2.imread(source_image, 0)          # queryImage
 
 # Initiate ORB detector
 orb = cv2.ORB()
 
-start_time = time.time()
-print "Picking up task: ", task_id, task_guid, youtube_id
-
-signal.signal(signal.SIGINT, partial(signal_handler, task_id, x, conn))
-signal.signal(signal.SIGTERM, partial(signal_handler, task_id, x, conn))
-
-filelist = os.listdir(youtube_path)
+filelist = os.listdir(video_path)
 for filename in sorted(filelist):
     if filename.endswith(".jpg"):
-        img2 = cv2.imread(youtube_path + filename, 0) # trainImage
+        img2 = cv2.imread(video_path + filename, 0) # trainImage
 
         frame, ext = filename.split(".")
 
@@ -91,27 +112,33 @@ for filename in sorted(filelist):
             # Sort them in the order of their distance.
             matches = sorted(matches, key = lambda x:x.distance)
 
-            #print youtube_id, frame, matches[0].distance, matches[0].trainIdx, matches[0].queryIdx, matches[0].imgIdx
+            #print video_id, frame, matches[0].distance, matches[0].trainIdx, matches[0].queryIdx, matches[0].imgIdx
 
             if matches[0].distance < 26: # only saving matches now
                 query = "INSERT INTO image_matches_bf (video_id, task_id, frame, filename, distance, trainIdx, queryIdx, imgIdx) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                args = (youtube_id, task_id, frame, filename, matches[0].distance, matches[0].trainIdx, matches[0].queryIdx, matches[0].imgIdx)
+                args = (video_id, task_id, frame, filename, matches[0].distance, matches[0].trainIdx, matches[0].queryIdx, matches[0].imgIdx)
                 x.execute(query, args)
                 #conn.commit()
 
         except Exception, e:
             print str(e)
-            print "Exception: ", youtube_path, filename, source_image, task_id, frame, filename
+            print "Exception: ", video_path, filename, source_image, task_id, frame, filename
             exceptions = exceptions + 1
             #query = "INSERT INTO image_matches_bf (video_id, task_id, frame, filename) VALUES (%s, %s, %s, %s)"
-            #args = (youtube_id, task_id, frame, filename)
+            #args = (video_id, task_id, frame, filename)
             #x.execute(query, args)
+
+# cleaning up
+print "[{0}] Cleaning up...".format(video_id)
+if video_id != "" and dl_required:
+    os.system("rm -f data/frames/" + video_id + "/*.jpg")
+    os.system("rmdir data/frames/" + video_id)
 
 query = "UPDATE tasks SET completed = %s, exceptions = %s WHERE task_id = %s LIMIT 1"
 args = (time.strftime('%Y-%m-%d %H:%M:%S'), exceptions, task_id)
 x.execute(query, args)
 
 run_time = (time.time() - start_time) / 60
-print "Finished task:   ", task_id, task_guid, youtube_id, run_time, " min"
+print "Finished task:   ", task_id, task_guid, video_id, run_time, " min"
 
 # allowing the script to die, respawned by match_forever.sh or restart options in docker-compose if running in prod
