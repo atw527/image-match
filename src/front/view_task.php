5cc22e3c3db64ca08ae9ff43205037b85d0f0875
Andrew Wells <andrew@wellsie.net>
2017-12-11 18:52:46 -0700

    Task viewer and other improvements

--- src/front/view_task.php
+++ src/front/view_task.php
@@ -41,6 +41,44 @@
         $match->s = $ts->s;
         $tasks[$row->task_id]->matches[] = $match;
     }
+
+    $sql = "SELECT count(*) as count, host FROM `tasks` WHERE guid = '$guid' GROUP BY host";
+    $query = $db->query($sql);
+    if (!$query) echo $sql;
+    $hosts = array();
+    while ($row = $query->fetch_object()) {
+        $hosts[] = array('count' => $row->count, 'host' => $row->host);
+    }
+
+    $sql = "SELECT video_id, min(distance) as min_dist FROM matches WHERE task_id IN ($ids) && distance < 10 GROUP BY task_id, video_id ORDER BY min_dist";
+    $query = $db->query($sql);
+    if (!$query) echo $sql;
+    $bests = array();
+    while ($row = $query->fetch_object()) {
+        $bests[] = $row->video_id;
+    }
+
+    $stats = array();
+    $sql = "SELECT count(*) as count FROM `tasks` WHERE task_id IN ($ids) && started IS NULL && completed IS NULL";
+    $query = $db->query($sql);
+    if (!$query) echo $sql;
+    while ($row = $query->fetch_object()) {
+        $stats['queued'] = $row->count;
+    }
+
+    $sql = "SELECT count(*) as count FROM `tasks` WHERE task_id IN ($ids) && started IS NOT NULL && completed IS NULL";
+    $query = $db->query($sql);
+    if (!$query) echo $sql;
+    while ($row = $query->fetch_object()) {
+        $stats['processing'] = $row->count;
+    }
+
+    $sql = "SELECT count(*) as count FROM `tasks` WHERE task_id IN ($ids) && started IS NOT NULL && completed IS NOT NULL";
+    $query = $db->query($sql);
+    if (!$query) echo $sql;
+    while ($row = $query->fetch_object()) {
+        $stats['completed'] = $row->count;
+    }
 ?>
 <html>
 <head>
@@ -57,21 +95,46 @@
             vertical-align: top;
         }
 
-        .task-image {
+        .task-image, .template-image {
             float: left;
         }
+
     </style>
 </head>
 <body>
 
-<a href="/new_task.php">New Task</a> | <a href="?guid=<?=$guid?>&distance=<?=$distance-1?>">Decrease Distance</a> | <a href="?guid=<?=$guid?>&distance=<?=$distance+1?>">Increase Distance</a>
+<a href="/tasks.php">&lt; All Tasks</a> | <a href="/new_task.php">New Task</a> | <a href="?guid=<?=$guid?>&distance=<?=$distance-1?>">Decrease Distance</a> | <a href="?guid=<?=$guid?>&distance=<?=$distance+1?>">Increase Distance</a>
 
 <hr />
 
-<img src="/data/templates/<?=$guid?>.jpg" width="400"/>
+<img class="template-image" src="/data/templates/<?=$guid?>.jpg" width="400" />
+
+<p class="meta">
+    Status<br />
+    <span class="label" style="font-weight: bold;"><?=$stats['queued']?></span><span class="value">Queued</span><br />
+    <span class="label" style="font-weight: bold;"><?=$stats['processing']?></span><span class="value">Processing</span><br />
+    <span class="label" style="font-weight: bold;"><?=$stats['completed']?></span><span class="value">Completed</span><br />
+</p>
+
+<p class="meta">
+    Worker Host Breakdown<br />
+    <?php foreach ($hosts as $host): ?>
+        <span class="label" style="font-weight: bold;"><?=$host['count']?></span>
+        <span class="value"><?=$host['host']?></span><br />
+    <?php endforeach; ?>
+</p>
+
+<p class="meta">
+    Matches < 10<br />
+    <?php foreach ($bests as $best): ?>
+        <a href="#<?=$best?>"><?=$best?></a> |
+    <?php endforeach; ?>
+</p>
+
+<div style="clear: both; padding: 5px; "></div>
 
 <?php foreach ($tasks as $task): ?>
-    <hr id="<?=$task->video_id?>" style="clear: both; " />
+    <hr style="clear: both; " />
 
     <a name="<?=$task->video_id?>" href="https://youtu.be/<?=$task->video_id?>" class="task-image" target="_blank"><img src="/data/video/<?=$task->video_id?>.jpg" width="300" /></a>
 
